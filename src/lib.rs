86fabc4a1e15b2cda5595ea049fa86b84e808297
Bhargava Sai Macha <mbsai29@gmail.com>
2022-09-16 17:03:47 +0530

    update to use mysql

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,37 @@
 use async_std::sync::{Mutex, MutexGuard};
 use diesel::{
     r2d2::{ConnectionManager, Pool, PooledConnection},
-    PgConnection,
+    mysql::MysqlConnection
 };
 use std::sync::Arc;
 use tide::{utils::async_trait, Middleware, Next, Request};
 
-pub type PooledPgConn = PooledConnection<ConnectionManager<PgConnection>>;
-pub type PoolPgConn = Pool<ConnectionManager<PgConnection>>;
+pub type PooledMySqlConn = PooledConnection<ConnectionManager<MysqlConnection>>;
+pub type PoolMySqlConn = Pool<ConnectionManager<MysqlConnection>>;
 
 #[derive(Clone)]
 pub struct DieselMiddleware {
-    pool: Pool<ConnectionManager<PgConnection>>,
+    pool: Pool<ConnectionManager<MysqlConnection>>,
 }
 
 impl DieselMiddleware {
     pub fn new(db_uri: &'_ str) -> std::result::Result<Self, Box<dyn std::error::Error>> {
-        let manager = ConnectionManager::<PgConnection>::new(db_uri);
-        let pg_conn = diesel::r2d2::Builder::<ConnectionManager<PgConnection>>::new()
+        let manager = ConnectionManager::<MysqlConnection>::new(db_uri);
+        let mysql_conn = diesel::r2d2::Builder::<ConnectionManager<MysqlConnection>>::new()
             .build(manager)
             .map_err(|e| Box::new(e))?;
-        Ok(Self { pool: pg_conn })
+        Ok(Self { pool: mysql_conn })
     }
 }
 
-impl AsRef<Pool<ConnectionManager<PgConnection>>> for DieselMiddleware {
-    fn as_ref(&self) -> &Pool<ConnectionManager<PgConnection>> {
+impl AsRef<Pool<ConnectionManager<MysqlConnection>>> for DieselMiddleware {
+    fn as_ref(&self) -> &Pool<ConnectionManager<MysqlConnection>> {
         &self.pool
     }
 }
 
-impl From<Pool<ConnectionManager<PgConnection>>> for DieselMiddleware {
-    fn from(pool: Pool<ConnectionManager<PgConnection>>) -> Self {
+impl From<Pool<ConnectionManager<MysqlConnection>>> for DieselMiddleware {
+    fn from(pool: Pool<ConnectionManager<MysqlConnection>>) -> Self {
         Self { pool }
     }
 }
@@ -42,11 +42,11 @@ where
     State: Clone + Send + Sync + 'static,
 {
     async fn handle(&self, mut req: Request<State>, next: Next<'_, State>) -> tide::Result {
-        if req.ext::<Arc<Mutex<PoolPgConn>>>().is_some() {
+        if req.ext::<Arc<Mutex<PoolMySqlConn>>>().is_some() {
             return Ok(next.run(req).await);
         }
 
-        let conn: Arc<PoolPgConn> = Arc::new(self.pool.clone());
+        let conn: Arc<PoolMySqlConn> = Arc::new(self.pool.clone());
         req.set_ext(conn.clone());
         let res = next.run(req).await;
         Ok(res)
@@ -55,24 +55,24 @@ where
 
 #[async_trait]
 pub trait DieselRequestExt {
-    async fn pg_conn<'req>(
+    async fn mysql_conn<'req>(
         &'req self,
-    ) -> std::result::Result<PooledPgConn, diesel::r2d2::PoolError>;
-    async fn pg_pool_conn<'req>(&'req self) -> MutexGuard<'req, PoolPgConn>;
+    ) -> std::result::Result<PooledMySqlConn, diesel::r2d2::PoolError>;
+    async fn mysql_pool_conn<'req>(&'req self) -> MutexGuard<'req, PoolMySqlConn>;
 }
 
 #[async_trait]
 impl<T: Send + Sync + 'static> DieselRequestExt for Request<T> {
-    async fn pg_conn<'req>(
+    async fn mysql_conn<'req>(
         &'req self,
-    ) -> std::result::Result<PooledPgConn, diesel::r2d2::PoolError> {
-        let pg_conn: &Arc<PoolPgConn> = self.ext().expect("You must install Diesel middleware");
-        pg_conn.get()
+    ) -> std::result::Result<PooledMySqlConn, diesel::r2d2::PoolError> {
+        let mysql_conn: &Arc<PoolMySqlConn> = self.ext().expect("You must install Diesel middleware");
+        mysql_conn.get()
     }
 
-    async fn pg_pool_conn<'req>(&'req self) -> MutexGuard<'req, PoolPgConn> {
-        let pg_conn: &Arc<Mutex<PoolPgConn>> =
+    async fn mysql_pool_conn<'req>(&'req self) -> MutexGuard<'req, PoolMySqlConn> {
+        let mysql_conn: &Arc<Mutex<PoolMySqlConn>> =
             self.ext().expect("You must install Diesel middleware");
-        pg_conn.lock().await
+        mysql_conn.lock().await
     }
 }
